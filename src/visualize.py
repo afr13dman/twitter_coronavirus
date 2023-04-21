b73456876dce822bfcf92325d1053e7da5f68a37
afr13dman <akfriedman13@gmail.com>
2023-04-20 20:47:42 -0700

    test

--- src/visualize.py
+++ src/visualize.py
@@ -30,9 +30,10 @@ for k,v in items:
     print(k,':',v)
 
 # plot bar graphs
-x_axis = [item[0] for item in sorted(items[:9])]
-y_axis = [item[1] for item in sorted(items[:9])]
-plt.bar(x_axis, y_axis)
+x_axis = [item[0] for item in items[:10]]
+y_axis = [item[1] for item in items[:10]]
+print(len(x_axis), len(y_axis))
+plt.bar(x_axis, sorted(y_axis))
 plt.ylabel("Count")
 if args.input_path == "reduced.country":
     plt.xlabel("Countries (Top 10)")
